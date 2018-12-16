7f9250b4c7de2762de30798b6870dd2b7ccd9d15
Chris Stephens <chrisjstephens@yahoo.com>
2018-12-15 20:43:59 -0500

    Dec 15 - Fix HeaderView Bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
 
 import './App.css';
-//import HeaderView from './components/HeaderView.react.js';
+import HeaderView from './components/HeaderView.react.js';
 import Flights from './components/Flights.react.js';
 import Home from './components/Home.react.js';
 import Hotels from './components/Hotels.react.js';
@@ -15,7 +15,7 @@ class App extends Component {
       <BrowserRouter basename="/projects/Zzpace-React/">
       <div className="app">
           <div className="container">
-            
+            <HeaderView/>
           </div>
           <div className="container">
             <div className="jumbotron rounded border p-3">
