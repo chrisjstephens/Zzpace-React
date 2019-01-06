e6bb637c3f8c17445a639b273075624aa2c4432c
Chris Stephens <chrisjstephens@yahoo.com>
2019-01-05 19:52:29 -0500

    Jan 5 - Fix Mobile Navbar Bug

--- src/components/HeaderView.react.js
+++ src/components/HeaderView.react.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default function HeaderView(props) {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Zzpace</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav" >
-        <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink className="nav-link"  to='/'>Home<span className="sr-only">(current)</span></NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName='active' to='/flights'>Flights</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to='/hotels'>Hotels</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to='/teleportation'>Teleportation</NavLink>
-        </li>
-        </ul>
-      </div>
-    </nav>
-  );
+
+export default class HeaderView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navCollapsed: true
+    };
+  }
+
+  onNavToggle = () => {
+    this.setState({ navCollapsed: !this.state.navCollapsed })
+  }
+
+  render() {
+    const navCollapsed = this.state.navCollapsed;
+
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">Zzpace</a>
+        <button className="navbar-toggler" type="button" onClick={this.onNavToggle} data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={(navCollapsed ? 'collapse navbar-collapse' : 'navbar-collapse')} id="navbarNav">
+          <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link"  to='/'>Home<span className="sr-only">(current)</span></NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" activeClassName='active' to='/flights'>Flights</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/hotels'>Hotels</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/teleportation'>Teleportation</NavLink>
+          </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
