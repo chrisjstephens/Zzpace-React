bb80d2dd731ecd50c456781522edf71190304ae7
chrisjstephens <chrisjstephens@yahoo.com>
2019-06-13 15:27:31 -0400

    June 13 - Add PlanetCards on Homepage

--- src/components/Home.react.js
+++ src/components/Home.react.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PlanetCards from './PlanetCards.react.js'
 
 export default function Hero(props) {
   return (
       <div>
         <h1 className="display-4">Welcome to Zzpace</h1>
         <p className="lead">We are your #1 source for intergalactic travel!</p>
-        <hr className="my-4"/>
         <p>We help you with flights, teleportation and hotels with the best prices available!</p>
-        <p className="lead">
-          <Link className="btn btn-primary btn-lg" to="#" role="button">Learn more</Link>
-        </p>
+        <hr className="my-4"/>
+        <PlanetCards />
       </div>
   );
 }
---
+++ src/components/PlanetCards.react.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class Hotels extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      planetCardsError: false
+    }
+
+    this.initData();
+  }
+
+  initData() {
+    fetch(process.env.REACT_APP_BACKEND_ADDRESS + "/api/showLocations")
+      .then(res => res.json())
+      .then(
+        (result) => this.setState({ data: result }),
+      ).catch((error) => {
+        this.setState({planetCardsError : true})
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      {!this.state.planetCardsError ?
+        <div className="planet-cards row no-gutters">
+          <h2>At Zzpace you can travel to many places including:</h2>
+          {
+            this.state.data
+           .sort(() => Math.random() - 0.5)
+           .slice(0, 3)
+           .map((res, index) =>
+              <div className="card col-md-4" key={res._id}>
+                <img className="card-img-top planet-picture" src={res.image} alt="Card image cap"/>
+                <div className="card-body">
+                  <h5 className="card-title">{res.name}</h5>
+                  <p className="card-text">{res.info}</p>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      : null}
+      </div>
+    );
+
+  }
+
+}
