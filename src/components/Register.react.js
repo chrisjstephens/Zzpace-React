7cbe8eb631401932e9cad12e03d487fb5d286885
Chris Stephens <chrisjstephens@yahoo.com>
2023-06-18 14:30:49 -0400

    June 18 - Change Register component to stateless

--- src/components/Register.react.js
+++ src/components/Register.react.js
@@ -1,33 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Alert from '@mui/material/Alert';
 import TextField from '@material-ui/core/TextField';
 
-export default class Register extends React.Component {
+export default function Register() {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [newUserError, setNewUserError] = useState('');
+  const [newUserCreated, setNewUserCreated] = useState(false);
 
-  constructor(props) {
-    super(props);
+  const validForm = username.length >= 8 && password.length >= 8;
 
-    this.state = {
-        username: '',
-        password: '',
-        newUserError: '',
-        newUserCreated: false
-    }
-  }
-
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
-
-  submitForm(e) {
+  const submitForm = (e) => {
     e.preventDefault();
 
     const formData = {
-      username: this.state.username,
-      password: this.state.password
+      username: username,
+      password: password
     }
 
     const requestOptions = {
@@ -36,34 +25,31 @@ export default class Register extends React.Component {
         body: JSON.stringify(formData)
     };
 
-    fetch(process.env.REACT_APP_BACKEND_ADDRESS + "/api/addUser", requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        return response.text().then(text => {
-          ////Error, perhaps duplicate entry
-          this.setState({newUserError : text});
-          this.setState({newUserCreated : false});
-        })
-      } else {
-        return response.text().then(text => {
-          //User created
-          this.setState({newUserError : ''});
-          this.setState({newUserCreated : true});
-        })
-      }
-      }).catch(err => {
-        //Error, perhaps server side
-        this.setState({newUserError : err});
-        this.setState({newUserCreated : false});
-      });
-  }
-
-  render() {
-    const validForm = this.state.username.length >= 8 && this.state.password.length >= 8;
+      fetch(process.env.REACT_APP_BACKEND_ADDRESS + "/api/addUser", requestOptions)
+        .then(response => {
+          if (!response.ok) {
+            return response.text().then(text => {
+              ////Error, perhaps duplicate entry
+              setNewUserError(text);
+              setNewUserCreated(false);
+            })
+          } else {
+            return response.text().then(text => {
+              //User created
+              setNewUserError('');
+              setNewUserCreated(true);
+            })
+          }
+          }).catch(err => {
+            setNewUserError(err);
+            setNewUserCreated(false);
+          });
+    
+  };
     
     return (
     <div>
-        <form noValidate onSubmit={this.submitForm.bind(this)} autoComplete="off">
+        <form noValidate onSubmit={submitForm} autoComplete="off">
             <div>
                 <h1>New User Registration</h1>
                 <p>Please register with a username and password!</p>
@@ -73,11 +59,11 @@ export default class Register extends React.Component {
                     <TextField
                         id="standard-username"
                         label="Username"
-                        value={this.state.username}
-                        onChange={this.handleChange('username')}
+                        value={username}
+                        onChange={(event) => setUserName(event.target.value)}
                         margin="normal"
-                        error={this.state.username !== '' && this.state.username.length < 8 ? true : false }
-                        helperText={this.state.username !== '' && this.state.username.length < 8 ? 'The username must be at least 8 characters!' : ''}
+                        error={username !== '' && username.length < 8 ? true : false }
+                        helperText={username !== '' && username.length < 8 ? 'The username must be at least 8 characters!' : ''}
                     />
                 </div>
             </div>
@@ -88,11 +74,11 @@ export default class Register extends React.Component {
                         label="Password"
                         type="password"
                         autoComplete="current-password"
-                        onChange={this.handleChange('password')}
-                        value={this.state.password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        value={password}
                         margin="normal"
-                        error={this.state.password !== '' && this.state.password.length < 8 ? true : false }
-                        helperText={this.state.password !== '' && this.state.password.length < 8 ? 'The password must be at least 8 characters!' : ''}
+                        error={password !== '' && password.length < 8 ? true : false }
+                        helperText={password !== '' && password.length < 8 ? 'The password must be at least 8 characters!' : ''}
                     />
                 </div>
             </div>
@@ -105,15 +91,15 @@ export default class Register extends React.Component {
             </div>
             <div className="form-row">
                <div className="form-group col-md-4">
-                   {this.state.newUserCreated ?
+                   {newUserCreated ?
                         <Alert severity="success">
-                        Congrats, a new user has been created! - {this.state.username}
+                        Congrats, a new user has been created! - {username}
                         </Alert>
                     : null 
                    }
-                   {this.state.newUserError ?
+                   {newUserError ?
                         <Alert severity="error">
-                        {this.state.newUserError}
+                        {newUserError}
                         </Alert>
                     : null
                    }
@@ -122,5 +108,4 @@ export default class Register extends React.Component {
         </form>
     </div>
     );
-  }
 }
